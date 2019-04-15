c5609f6b29b91e45889946a64c3d7a1e4e451768
NataliaGracheva <40542294+NataliaGracheva@users.noreply.github.com>
2019-04-15 20:22:15 +0500

    Update cart.js

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -3,12 +3,11 @@ const itemsList = document.querySelector('.items-list');
 
 itemsList.addEventListener('click', function(event) {
   
-  if (!event.target.classList.contains('add-to-cart')) {
-  	return;
+  if (event.target.classList.contains('add-to-cart')) {
+  	event.preventDefault();
+    addToCart({
+    title: event.target.dataset.title,
+    price: event.target.dataset.price
+    });
   }
-  const item = {};
-  item.title = event.target.dataset.title;
-  item.price = event.target.dataset.price;
-  addToCart(item);
 });
-
